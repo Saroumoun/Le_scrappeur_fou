3c9a1a4a6b044f74a4998880766f47d811858bdb
apouille <nguon.melanie@gmail.com>
2019-01-17 23:01:00 +0100

    AJout commentaires

--- lib/deputy.rb
+++ lib/deputy.rb
@@ -1,19 +1,23 @@
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+
+#1 Première méthode : Déclaration de la page à scrapper
 def get_page
 	page = Nokogiri::HTML(open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=600&lang=fr"))
 	return page
 end 
 
+#2 Deuxième méthode : Collecte des emails des députés
 def scrapp_emails
 	page = get_page
 	emails_array = []
-	bureau = "bureau-m-orphelin@assemblee-nationale.fr"
+	bureau = "bureau-m-orphelin@assemblee-nationale.fr" #/ on met à l'écart deux mails en doublons identifiés
 	secretariat = "secretariat-blanchet@assemblee-nationale.fr"
 
-	emails = page.xpath('//*[contains(text(), "@assemblee-nationale.fr")]')
+	emails = page.xpath('//*[contains(text(), "@assemblee-nationale.fr")]') 
 	
 	emails.each do |email|
 		emails_array << email.text.strip unless email.text.strip == bureau || email.text.strip == secretariat
@@ -21,25 +25,29 @@ def scrapp_emails
 	return emails_array
 end
 
+#3 Troisième méthode : Collecte des noms et prénoms des députés
 def scrapp_names
 	page = get_page
 	full_names_array = []
 
-	page.xpath('//*[@class="titre_normal"]').each do |the_name|
+	page.xpath('//*[@class="titre_normal"]').each do |the_name| #/ on enlève le Madame, Monsieur en ne prenant que le deuxième et troisième bloc de la string
 		full_names_array << { "first_name" => the_name.text.split(" ")[1], "last_name" => the_name.text.split(" ")[2]}
 	end
 	return full_names_array		
 end
 
+#4 Quatrième méthode : Synchronisation des noms et emails des députés
 def join_name_and_email
 	full_names_array = scrapp_names
 	emails_array = scrapp_emails
-
-	full_names_array.map { |v| v["email"] = ""; v }
-    full_names_array.map.with_index { |hash, i| hash["email"] = emails_array[i]; hash }
+	join_array = []
+	# (full_names_array.map { |v| v["email"] = ""; v } -- code facultatif)
+    full_names_array.map.with_index { |hash, i| hash["email"] = emails_array[i]; hash } 
+    #/ pour chaque nom et prénom de députés, on associe une adresse mail pour former un hash
 	puts full_names_array
-	return full_names_array       
+	return full_names_array  
 end
+
 
 join_name_and_email
 
--- lib/townhall.rb
+++ lib/townhall.rb
@@ -1,42 +1,43 @@
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-def get_page
-	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-	return page
-end 
 
+#1 Première méthode : Collecte de l'email d'une mairie d'une ville du Val d'Oise
 def get_townhall_email(townhall_url)
-	a = []
-	page = Nokogiri::HTML(open(townhall_url))
+	page = Nokogiri::HTML(open(townhall_url)) #/ on indique un site URL neutre qui sera indiqué dans la prochaine méthode
+	email_array = []
 
 	email = page.xpath('//*[contains(text(), "@")]').text
-	town = page.xpath('//*[contains(text(), "Adresse mairie de")]').text.split
+	town = page.xpath('//*[contains(text(), "Adresse mairie de")]').text.split #/ on divise la string pour pouvoir récupérer uniquement le nom de la ville
 
-	a << {town[3] => email}
-	puts a
-	return a
+	email_array << {town[3] => email} #/ on indique la position du nom de la ville dans la string pour la récupérer
+	puts email_array
+	return email_array
 end
 
+#2 Deuxième méthode : Collecte de toutes les URLs des villes du Val d'Oise
 def get_townhall_urls
+	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 	url_array = []
-	page = get_page
 
-	urls = page.xpath('//*[@class="lientxt"]/@href')
+	urls = page.xpath('//*[@class="lientxt"]/@href') #/ toutes les URLs appartiennent à la classe lientxt
 
-	urls.each do |url|
-		url = "http://annuaire-des-mairies.com" + url.text[1..-1]
+	urls.each do |url| #/ pour chaque URLs récupérées, il faut leur indiquer l'url parent "http://annuaire-des-mairies.com"
+		url = "http://annuaire-des-mairies.com" + url.text[1..-1] #/ A l'url parent, on ajoute les urls récupérées du deuxième caractère au dernier caractère, car on veut se débarasser du point devant.
 		url_array << url		
 	end
 	return url_array 
 end
 
-def scrapp
-	url_array = get_townhall_urls
-	url_array.each do |townhall_url|
+#3 Troisième méthode : Synchronisation des noms des villes et des emails des mairies
+def scrapp_data
+	url_array = get_townhall_urls 
+	url_array.each do |townhall_url| #/ pour chaque URL d'une ville du Val d'Oise, on associe l'adresse mail de la mairie
 		get_townhall_email(townhall_url)
 	end
 end 
 
-scrapp
+
+scrapp_data
